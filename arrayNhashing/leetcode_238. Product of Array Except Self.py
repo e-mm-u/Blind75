c339f6d56b227c40336ad46640c6b55824ff5c71
e-mm-u <uh.habiba1012@gmail.com>
2022-11-16 13:37:45 +0600

    removed unnecessary lines

--- arrayNhashing/leetcode_238. Product of Array Except Self.py
+++ arrayNhashing/leetcode_238. Product of Array Except Self.py
@@ -17,14 +17,5 @@ def fnc(nums):
     print(res)
 
 
-    l = len(nums)
-    res = [1 for i in range(l)]
-    res[0] = 1
-    for i in range(1,l,1):
-        res[i] = res[i-1]*nums[i-1]
-
-    for i in range(l-1,0,-1)
-
-
 nums = [1,2,3,4,5]
 fnc(nums)
